06c7b57f9312c5739667715c5cddcc36aa5dc26a
Ahmad Yasser <42544598+ahmdyasser@users.noreply.github.com>
2022-02-03 10:37:48 +0200

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Ahmad Yasser on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
